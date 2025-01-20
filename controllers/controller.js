a6519b4caba1e217b5bfb26d6b7dfa28fb75f6cc
code-hopper <ameykhondekar01@gmail.com>
2025-01-20 10:03:20 +0530

    created table to display student and also added logics to check for duplicate entries and missing fields

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,42 @@
 import "../Database/conn.js"
 import { studentModel } from "../models/studentSchema.js"
 
-let getHome = (req, res) => {
+let statusMessage = false
+
+let setStatusMessage = (message) => {
+    statusMessage = message
+}
+
+let getHome = async (req, res) => {
     console.log("get home route")
-    res.status(200).render("index")
+
+    try {
+
+        let data = await fetchStudentsData()
+
+        res.status(200).render("index", { message: statusMessage, data })
+
+    } catch (err) {
+        console.log("error while loading the data / fetch data error : ", err)
+        res.status(400).render("index", { message: "error while loading the data / fetch data error", data: false })
+    }
+}
+
+async function fetchStudentsData() {
+    try {
+
+        let result = await studentModel.find()
+
+        if (result.length != 0) {
+            return result
+        } else {
+            return false
+        }
+
+    } catch (err) {
+        console.log("unable to fetch students data : ", err)
+        return false
+    }
 }
 
 let postHome = async (req, res) => {
@@ -15,17 +48,42 @@ let postHome = async (req, res) => {
         // use studentModel to insert the data into the students collection
         // first create a new model then use save method
 
-        let studentEntry = new studentModel(data)
+        let { name, phone, email, dob, address, prev_education, stream, course_instrested, entrance_marks } = data
 
-        let result = await studentEntry.save()
+        // check if any field is missing
 
-        console.log("entry saved into database = ", result)
+        if (!name || !phone || !email || !dob || !address || !prev_education || !stream || !course_instrested || !entrance_marks) {
+            console.log("not a valid data i.e. a field might be missing !")
+            setStatusMessage("not a valid data i.e. a field might be missing !")
+            res.status(400).redirect("/")
+        } else {
 
-        res.status(202).redirect("/")
+            // check that there should be no repeat email and phone
 
+            let checkDuplicate = await studentModel.findOne({ $or: [{ email: email }, { phone: phone }] })
+
+            if (!checkDuplicate) {
+
+                let studentEntry = new studentModel(data)
+
+                await studentEntry.save()
+
+                setStatusMessage(false)
+
+                res.status(202).redirect("/")
+
+            } else {
+                console.log("tried to make a duplicate entry !")
+                console.log(checkDuplicate)
+                setStatusMessage("duplicate entry please change email / phone !")
+                res.status(400).redirect("/")
+            }
+
+        }
     } catch (err) {
         console.log("unable to post the student entry data")
         console.log(err)
+        setStatusMessage("error while saving the data")
         res.status(400).redirect("/")
     }
 }
